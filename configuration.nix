74d874561da7dc603513c3e9c286881761cd86aa
phanirithvij <phanirithvij2000@gmail.com>
2024-03-25 16:16:03 +0530

    Revert "try pretend"
    This reverts commit 20d8dfd613b745089b50380b5889452551134c24.

--- configuration.nix
+++ configuration.nix
@@ -45,10 +45,6 @@
 
   networking.hostName = "iron";
   networking.networkmanager.enable = true;
-  networking.extraHosts =
-  ''
-    127.0.0.1 nightly.changelog.com
-  '';
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
