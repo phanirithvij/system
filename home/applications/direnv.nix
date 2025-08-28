4ab74a33629d0644b80cb788e82252c6c877fb3a
phanirithvij <phanirithvij2000@gmail.com>
2025-08-28 09:50:46 +0530

    hm direnv application
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ home/applications/direnv.nix
@@ -0,0 +1,11 @@
+_: {
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    config = {
+      global.disable_stdin = true;
+      global.strict_env = true;
+      global.hide_env_diff = true;
+    };
+  };
+}
