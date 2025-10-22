795f259ed63ec405bdbf3860bc14f4412bcbe3cb
phanirithvij <phanirithvij2000@gmail.com>
2025-10-22 11:32:21 +0530

    hm git change options
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/git/default.nix
+++ home/applications/git/default.nix
@@ -14,30 +14,24 @@
     ./lazygit.nix
   ];
   programs = {
+    delta.enable = true;
+    delta.enableGitIntegration = true;
     git = {
       enable = true;
       package = pkgs.gitFull;
-      userName = "phanirithvij";
-      userEmail = "phanirithvij2000@gmail.com";
-      # broken https://github.com/NixOS/nixpkgs/pull/334814
-      delta.enable = true;
       signing.format = "ssh";
       signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF8/+FK1TAZV7p1a92/ykOXqPGt34rsiHxXLgVG3b/3x rithvij@iron";
       signing.signByDefault = true;
-      extraConfig = {
+      settings = {
+        user.name = "phanirithvij";
+        user.email = "phanirithvij2000@gmail.com";
         commit.gpgsign = true;
         gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
-        init = {
-          defaultBranch = "main";
-        };
-        url = {
-          "https://github.com/" = {
-            insteadOf = [
-              "gh:"
-              "github:"
-            ];
-          };
-        };
+        init.defaultBranch = "main";
+        url."https://github.com/".insteadOf = [
+          "gh:"
+          "github:"
+        ];
       };
     };
   };
