9a0d6981999335655300223c271e142d5e0c3e4b
phanirithvij <phanirithvij2000@gmail.com>
2025-12-08 20:53:58 +0530

    WIP
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/lazy/scripts/init.sh
+++ pkgs/lazy/scripts/init.sh
@@ -3,15 +3,15 @@
 set -x
 
 SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-APPLICATIONS_DIR="$SCRIPT_DIR"/../desktopItems
-TMPDIR_APPS="$(mktemp -d -t "lazy-apps-dir-XXXX")"
+#APPLICATIONS_DIR="$SCRIPT_DIR"/../desktopItems
+#TMPDIR_APPS="$(mktemp -d -t "lazy-apps-dir-XXXX")"
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 # see https://github.com/NixOS/nix/issues/7076
 # nix-cli works not `nix cli` for passing --argstr
 built=$(
   nom-build \
-    "$SCRIPT_DIR/bundle-lazy-apps.nix" \
+    "$SCRIPT_DIR/bundle-original-apps.nix" \
     --argstr gitRoot "$ROOT_DIR" \
     --no-out-link \
     --allow-import-from-derivation #\
@@ -20,8 +20,8 @@ built=$(
 
 test $? = 0 || exit 1
 echo "$built"
-cp -r --no-preserve=all "$built"/* "$TMPDIR_APPS"
+#cp -r --no-preserve=all "$built"/* "$TMPDIR_APPS"
 
-mkdir -p "$APPLICATIONS_DIR"
-cp -rL "$TMPDIR_APPS"/* "$APPLICATIONS_DIR"
-rm -rf "$TMPDIR_APPS"
+#mkdir -p "$APPLICATIONS_DIR"
+#cp -rL "$TMPDIR_APPS"/* "$APPLICATIONS_DIR"
+#rm -rf "$TMPDIR_APPS"
