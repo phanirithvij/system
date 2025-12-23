973a993b9164dd6fb8edf4491821a7a0d7892c91
phanirithvij <phanirithvij2000@gmail.com>
2025-12-23 11:26:01 +0530

    try debug mnt warning
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
    
    'github:phanirithvij/nixpkgs/1235be65208273a845717f4d885a3b8ff7ac777d?narHash=sha256-fhxcfcYYSj9VcIHYGeqV3b96GFVXhi3ixEmp7FRtAJQ%3D'
    (2025-06-25)
      →
    'github:phanirithvij/nixpkgs/ef86ba0384d2403b807d1835efa1913be39b2e2d?narHash=sha256-YMJovVtzFP9aeMxeRak9UiNszScf%2BVGkwiV/y0xkYdA%3D'
    (2025-06-25)

--- flake.nix
+++ flake.nix
@@ -105,6 +105,9 @@
     # octotail package (mine)
     nixpkgs-patch-419929.url = "https://github.com/NixOS/nixpkgs/pull/419929.patch?full_index=1";
     nixpkgs-patch-419929.flake = false;
+    # debug stage-1 boot issue
+    nixpkgs-patch-10.url = ./nixos/patches/0001-try-debug-mnt-issue.patch;
+    nixpkgs-patch-10.flake = false;
 
     # TODO disabling for now because of rl-2511 notes conflict
     # opengist module (mine, its complex with createDBLocal etc.)
