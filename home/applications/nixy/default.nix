2385fb76e236c53e2b41c94fc81417044e3b2858
phanirithvij <phanirithvij2000@gmail.com>
2025-12-23 21:52:28 +0530

    add nix-patcher to hm
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/nixy/default.nix
+++ home/applications/nixy/default.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  flake-inputs,
+  system,
+  ...
+}:
 {
   imports = [ ./nix.nix ];
   home.packages = with pkgs; [
     compose2nix
     #pr-tracker #should be autoadded via nurpkgs
     nurPkgs.flakePkgs.yaml2nix
+    flake-inputs.nix-patcher.packages.${system}.nix-patcher
   ];
 }
