559721d52f9a5a771f27d010643b9984fee7f5b6
phanirithvij <phanirithvij2000@gmail.com>
2026-01-08 18:50:39 +0530

    move linux-firmware-iron-zstd to nur-pkgs
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- hosts/iron/hardware-configuration.nix
+++ hosts/iron/hardware-configuration.nix
@@ -37,29 +37,8 @@
 
   hardware.firmware = with pkgs; [
     (compressFirmwareZstd wireless-regdb)
-    (stdenv.mkDerivation {
-      phases = [
-        "unpackPhase"
-        "installPhase"
-      ];
-      name = "linux-firmware-filtered";
-      src = compressFirmwareZstd linux-firmware; # can get this from official nixos cache
-      installPhase = ''
-        runHook preInstall
-        mkdir -p $out/lib/firmware
-        pushd $src/lib/firmware || exit 1
-        files=(
-          iwlwifi-7265D-29.ucode*
-          i915/kbl_dmc_ver1_04.bin*
-          intel/ibt-hw-37.8.10-fw-1.10.3.11.e.bseq*
-          intel/ibt-hw-37.8.bseq*
-        )
-        cp --no-preserve=mode --parents ''${files[@]} $out/lib/firmware
-        chmod -w -R $out/lib/firmware
-        popd || exit 1
-        runHook postInstall
-      '';
-    })
+    pkgs.nurPkgs.linux-firmware-iron-zstd # custom filtered firmware files
+    # (compressFirmwareZstd linux-firmware) # original nixpkgs equivalent
   ];
 
   boot.initrd.availableKernelModules = [
