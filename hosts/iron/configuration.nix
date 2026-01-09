224071e44093cd25c6b9a37e71342007bf889db7
phanirithvij <phanirithvij2000@gmail.com>
2026-01-09 09:22:33 +0530

    WIP try invoiceplane
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- hosts/iron/configuration.nix
+++ hosts/iron/configuration.nix
@@ -40,6 +40,7 @@ in
     ../../nixos/applications/ntfy-rss.nix
     # ../../nixos/applications/opengist.nix
     ../../nixos/applications/reaction
+    ../../nixos/applications/selfhosted/invoiceplane.nix
     ../../nixos/applications/selfhosted/jackett.nix
     ../../nixos/applications/selfhosted/mariadb-dolphie.nix
     ../../nixos/applications/selfhosted/qbittorrent.nix
---
+++ nixos/applications/selfhosted/invoiceplane.nix
@@ -0,0 +1,7 @@
+_: {
+  services.invoiceplane = {
+    sites.localhost = {
+      enable = true;
+    };
+  };
+}
