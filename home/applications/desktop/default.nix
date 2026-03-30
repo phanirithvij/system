6667ed6feb84a8b5f8a2be75105475044ab403ec
phanirithvij <phanirithvij2000@gmail.com>
2026-03-30 18:41:35 +0530

    hm: add jitsi-meet-electron to desktop apps
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/desktop/default.nix
+++ home/applications/desktop/default.nix
@@ -3,5 +3,6 @@
   imports = lib.filter (x: x != ./default.nix) (lib.filesystem.listFilesRecursive ./.);
   home.packages = [
     pkgs.lazyPkgs.simplescreenrecorder
+    pkgs.jitsi-meet-electron
   ];
 }
