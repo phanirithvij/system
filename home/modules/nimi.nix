8f77c621b1a08b783ba009646ff736b779564b48
phanirithvij <phanirithvij2000@gmail.com>
2026-05-22 10:13:34 +0530

    backrest via nimi
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/modules/nimi.nix
+++ home/modules/nimi.nix
@@ -1,12 +1,23 @@
 {
-  flake-inputs,
   pkgs,
+  config,
+  flake-inputs,
   ...
 }:
 {
   imports = [
     flake-inputs.nimi.homeModules.default
   ];
+
+  nimi.backrest = {
+    services.backrest = {
+      imports = [ pkgs.backrest.services.default ];
+      backrest.dataDir = "${config.home.homeDirectory}/.local/share/backrest";
+      backrest.configPath = "${config.home.homeDirectory}/.config/backrest/config.json";
+    };
+    settings.restart.mode = "up-to-count";
+    settings.restart.time = 2000;
+  };
 
   nimi.timetagger = {
     services.timetagger = {
