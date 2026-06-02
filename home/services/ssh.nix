9ee4ea0ceb50788ee7c309584aae0b800280f22a
phanirithvij <phanirithvij2000@gmail.com>
2026-06-02 18:06:49 +0530

    try ssh-add on login
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/services/ssh.nix
+++ home/services/ssh.nix
@@ -1,3 +1,24 @@
+{ pkgs, ... }:
+
 {
   services.ssh-agent.enable = true;
+
+  systemd.user.services.ssh-add = {
+    Unit = {
+      Description = "Add SSH key to ssh-agent";
+      After = [ "ssh-agent.service" ];
+      Requires = [ "ssh-agent.service" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.openssh}/bin/ssh-add %h/.ssh/id_ed25519";
+      Environment = "SSH_AUTH_SOCK=%t/ssh-agent";
+      RemainAfterExit = true;
+      Restart = "on-failure";
+      RestartSec = 2;
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
