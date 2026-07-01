5b41c1c6b7e00f289e076e83e4a5f0434506c987
phanirithvij <phanirithvij2000@gmail.com>
2026-07-01 12:50:12 +0530

    try https
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/applications/selfhosted/pixelfed.nix
+++ nixos/applications/selfhosted/pixelfed.nix
@@ -7,20 +7,19 @@
   services.pixelfed = {
     enable = true;
     domain = "iron.tail4aa8d.ts.net";
-    nginx.listen = [
-      {
-        port = 9393;
-        addr = "0.0.0.0";
-      }
-    ];
+    nginx = {
+      forceSSL = true;
+      sslCertificate = "/etc/ssl/certs/pixelfed.crt";
+      sslCertificateKey = "/etc/ssl/private/pixelfed.key";
+    };
     # not a real secret
     secretFile = pkgs.writeText "dont-do-it-like-this" ''
       APP_KEY="base64:x/cMhKq1nL8e2V0rA5zP4vG7tB9wD2xF5yH8sJ1kMNo="
     '';
     settings = {
-      "FORCE_HTTPS_URLS" = false;
-      "APP_URL" = "http://iron.tail4aa8d.ts.net:9393";
-      "SESSION_SECURE_COOKIE" = false;
+      "FORCE_HTTPS_URLS" = true;
+      "APP_URL" = "https://iron.tail4aa8d.ts.net";
+      "SESSION_SECURE_COOKIE" = true;
       "OPEN_REGISTRATION" = lib.mkForce true;
       "MAIL_DRIVER" = "log";
       "MAIL_MAILER" = "log";
