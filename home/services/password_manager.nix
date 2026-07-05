7201b66fec3fdfb27127f6e0770a3d7e92bdcae2
phanirithvij <phanirithvij2000@gmail.com>
2026-07-05 12:12:08 +0530

    keepassxc moved to nurpkgs
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>
    
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
    
    'github:phanirithvij/nixpkgs/f7803518156e46a0a77edc2061e62bcdf8593cc5?narHash=sha256-q%2Bnc83EaoS8bIOL1KD4guBAXGioVT85UKSPuHx34R6M%3D'
    (2026-07-04)
      →
    'github:phanirithvij/nixpkgs/c8c9a96965ad3ea4e5937576791bba47c5f7e1a9?narHash=sha256-uVZinqdojMhJ6zWYsoV7Z9CKv4kNOe4pBBRTWaatJUY%3D'
    (2026-06-28)
    
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nur-pkgs':
    
    'github:phanirithvij/nur-packages/1f295ddf6ae3cf9eeb6e0337e4f6712f37c1a342?narHash=sha256-A%2BlPRmpe5VE6n30GLkn%2BMZ5dD7hJAoL0H7UykKN/sg4%3D'
    (2026-06-28)
      →
    'github:phanirithvij/nur-packages/8fda6ab331628dbb8551dea91a8eb5d9ae1579e5?narHash=sha256-enQGvDq603WhKIyNY06hz3tCaL9wtiDOO9HvMHMNHko%3D'
    (2026-07-05)

--- home/services/password_manager.nix
+++ home/services/password_manager.nix
@@ -2,6 +2,7 @@
 
 # https://reddit.com/r/KeePass/comments/1bor6zm/comment/kwsp65m
 # https://keepassxc.org/docs/KeePassXC_UserGuide#_automatic_database_opening
-{
+{ pkgs, ... }: {
   programs.keepassxc.enable = true;
+  programs.keepassxc.package = pkgs.nurPkgs.keepassxc;
 }
